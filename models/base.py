7cd507a8943ea2f4b3be446132c17c5a1c01af03
rudyluis <68257300+rudyluis@users.noreply.github.com>
2025-06-07 19:11:49 -0400

    Update base.py

--- models/base.py
+++ models/base.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # ¡MUY IMPORTANTE!: Reemplaza 'user', 'password' y 'your_database_name' con tus credenciales reales.
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/superstore"
+DATABASE_URL = "postgresql://dbgame_sfhn_user:5UEkhDNGJaMQfAa5sNfHlZbPrFkoGCGF@dpg-d0llaopr0fns738g24og-a.oregon-postgres.render.com/dbgame_sfhn"
 
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
